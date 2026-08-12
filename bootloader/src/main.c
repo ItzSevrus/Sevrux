83186c7a7a710d7e6a031f41c5f1bbfbca6cc873
ItzSevrus <306717925+ItzSevrus@users.noreply.github.com>
2026-08-12 20:16:07 +0530

    bootloader source

---
+++ bootloader/src/main.c
@@ -0,0 +1,13 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  InitializeLib(ImageHandle, SystemTable);
+
+  Print(L"Welcome to Sevrux!\r\n");
+
+  while (1)
+    ;
+
+  return EFI_SUCCESS;
+}
